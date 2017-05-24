c06ea8f757abcbf3dc4b998168c6d8d7e0690801
Alexandre Lemaire <saeven@saeven.net>
2017-05-24 17:20:35 -0400

    Added MOTD & Connection Startup. Should probably split the lists into authenticating, and active.

--- src/saevenx/connection.go
+++ src/saevenx/connection.go
@@ -4,32 +4,63 @@ import (
 	"net"
 	"time"
 	"bufio"
+	"strings"
 )
 
+const STATE_WELCOME = 0
+const STATE_LOGIN_USERNAME = 1
+const STATE_LOGIN_PASSWORD = 2
+const STATE_LOGIN_MENU = 3
+
 type Connection struct {
-	conn net.Conn
+	conn          net.Conn
 	timeConnected time.Time
-	state int8
-	server *Server
-	player *Player
+	state         int8
+	username      string
+	server        *Server
+	player        *Player
 }
 
 func (connection *Connection) Write(message string) {
 	connection.conn.Write([]byte(message))
 }
 
+func (connection *Connection) sendMOTD() {
+	connection.Write(connection.server.Motd)
+	connection.Write("What is your name, mortal?")
+}
+
 func (connection *Connection) listen() {
 	reader := bufio.NewReader(connection.conn)
 
+	connection.sendMOTD()
+	connection.state = STATE_LOGIN_USERNAME
+
 	for {
 		message, err := reader.ReadString('\n')
+
 		if err != nil {
 			connection.conn.Close()
 			connection.server.onClientConnectionClosed(connection, err)
 			return
 		}
+
+		message = strings.TrimSpace(message)
+
+		switch connection.state {
+
+		case STATE_LOGIN_USERNAME:
+			connection.state = STATE_LOGIN_PASSWORD
+			connection.username = message
+			connection.Write("Your password?")
+
+		case STATE_LOGIN_PASSWORD:
+			connection.state = STATE_LOGIN_MENU
+			connection.Write("Welcome. Death Awaits.\n")
+
+		}
+
 		connection.server.onMessageReceived(connection, message)
 
 	}
 }
-
--- src/saevenx/server.go
+++ src/saevenx/server.go
@@ -4,21 +4,32 @@ import (
 	"net"
 	"time"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type Server struct {
 	connectionList []*Connection
 	ticker         *time.Ticker
+	Motd		string
 }
 
 func CreateServer() *Server {
 	server := &Server{}
 	server.connectionList = make([]*Connection, 0)
+	pwd, _ := os.Getwd()
+
+	fmt.Println(pwd)
+
+	fmt.Println("[CONFIG] Pulling MOTD")
+	bytes, _ := ioutil.ReadFile(pwd + "/resources/MOTD")
+	server.Motd = string(bytes)
+
 	return server
 }
 
 func (server *Server) AddConnection(connection net.Conn) *Connection {
-	newConnection := Connection{conn: connection, timeConnected: time.Now(), server: server}
+	newConnection := Connection{conn: connection, timeConnected: time.Now(), server: server, state: STATE_WELCOME}
 	server.connectionList = append(server.connectionList, &newConnection)
 	go newConnection.listen()
 	fmt.Printf("[CONN] There are %d connected users.\n", server.ConnectionCount())
@@ -45,13 +56,12 @@ func (server *Server) onClientConnectionClosed(connection *Connection, err error
  * Start the main game loop
  */
 func (server *Server) Start() {
-	server.ticker = time.NewTicker(time.Millisecond * 500)
+	server.ticker = time.NewTicker(time.Millisecond * 3000)
 
 	go func() {
 		for range server.ticker.C {
-
 			for _, c := range server.connectionList {
-				c.Write(".")
+				fmt.Printf("[TICK] Running update tick on player (%s) at state [%d]\n", c.username, c.state )
 			}
 		}
 	}()
